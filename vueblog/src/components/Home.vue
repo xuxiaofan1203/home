1f0bacb3e112e15deb87aca26a8840d75eed776e
Xu Xiaofan <xuxiaofan904@163.com>
2024-06-14 19:52:17 +0800

    modify title name

--- vueblog/src/components/Home.vue
+++ vueblog/src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <el-container class="home_container">
     <el-header>
-      <div class="home_title">V部落博客管理平台</div>
+      <div class="home_title">简单的博客管理平台</div>
       <div class="home_userinfoContainer">
         <el-dropdown @command="handleCommand">
   <span class="el-dropdown-link home_userinfo">
